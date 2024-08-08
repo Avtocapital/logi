4797766192c88ecde152816816f980bc16069fc7
Avtocapital <152841945+Avtocapital@users.noreply.github.com>
2024-08-08 15:07:30 +0500

    Til almawtirish funksiyalari qoshildi
    saytdigi kontentni 3 xil tilga ozgartiradigan funksiya qoshilgan

--- assets/js/main.js
+++ assets/js/main.js
@@ -143,3 +143,103 @@
   });
 
 })();
+
+//language switcher 
+
+const translations = {
+  en: {
+      "home": "Home",
+      "about": "About",
+      "contact": "Contact",
+      "welcome": "Welcome to MA GLOBAL GROUP",
+      "description": "We provide reliable cargo transportation services.",
+      "about_description": "Details about the company's main activities.",
+      "name": "Name:",
+      "email": "Email:",
+      "message": "Message:",
+      "subject": "Subject",
+      "send": "Send",
+      "order-btn" : "Act to call",
+      "export-import": "Export import",
+      "inter-logistic": "International logistics",
+      "brand-distribution": "Distribution differnet brands",
+      "export-desc": "grains and legumes, equipment, building materials",
+      "logist-desc" : "trucks, car transporters, railway, sea transportation and air",
+      "distribut-desc": "cosmetics, cereal brands",
+      "about-desc" : "MA GLOBAL GROUP provides a wide range of services in the field of freight transportation, customs clearance and cargo handling.By providing strategic supply chain management, we increase our customers' competitiveness by making their logistics as efficient as possible.",
+      "Our-services" : "Our services",
+      "Our-desc": "Services provided by our company",
+      "order-text": "Submit your application",
+      "order-desc": "Send us a request and get a consultation",
+      "faq-title": "FAQ",
+      "contact-title": "Contact us",
+      "contact-desc": "Contact with us"
+  },
+  ru: {
+      "home": "Главная",
+      "about": "О нас",
+      "contact": "Контакты",
+      "welcome": "Добро пожаловать в MA GLOBAL GROUP",
+      "description": "Мы предоставляем надежные услуги по грузоперевозкам.",
+      "about_description": "Подробности о основной деятельности компании.",
+      "name": "Имя:",
+      "email": "Электронная почта:",
+      "message": "Сообщение:",
+      "subject": "Тема",
+      "send": "Отправить",
+      "order-btn" : "Оставить заявку",
+      "export-import": "Экспорт импорт",
+      "inter-logistic": "Международная логистика",
+      "brand-distribution": "Дистрибутор разных брендов",
+      "export-desc": "зерновые и бобовые культуры, оборудование, строительные материалы",
+      "logist-desc" : "фуры, автовозы, ж/д, морская перевозка и авиа",
+      "distribut-desc": "косметика, зерновых брендов",
+      "about-desc" : "MA GLOBAL GROUP оказывает широкий спектр услуг в области грузовых перевозок, таможенного оформления и обработки грузов.Обеспечивая стратегическое управление цепочками поставок, мы повышаем конкурентоспособность наших клиентов, делая их логистику максимально эффективной.",
+      "Our-services" : "Наши услуги",
+      "Our-desc": "Услуги которые предоставляет наша компания",
+      "order-text": "Оставить заявку",
+      "order-desc": "Отправьте нам запрос и получите консультацию",
+      "faq-title": "Часто задаваемые вопросы",
+      "contact-title": "Свяжитесь с нами",
+      "contact-desc": "Свяжитесь с нами"
+  },
+  uz: {
+      "home": "Bosh sahifa",
+      "about": "Biz haqimizda",
+      "contact": "Aloqa",
+      "welcome": "MA GLOBAL GROUP ga xush kelibsiz",
+      "description": "Biz ishonchli yuk tashish xizmatlarini taqdim etamiz.",
+      "about_description": "Kompaniyaning asosiy faoliyati haqida batafsil ma'lumot.",
+      "name": "Ism:",
+      "email": "Elektron pochta:",
+      "message": "Xabar:",
+      "subject": "Mavzu",
+      "send": "Yuborish",
+      "order-btn" : "Ariza yuborish",
+      "export-import": "Eksport import",
+      "inter-logistic": "Xalqaro logistika",
+      "brand-distribution": "Turli brendlar distributori",
+      "export-desc": "don va dukkakli ekinlar, asbob-uskunalar, qurilish materiallari",
+      "logist-desc" : "yuk mashinalari, avtomobil tashuvchilar, temir yo'l, dengiz transporti va havo yo'llari",
+      "distribut-desc": "kosmetika, donli brendlar",
+      "about-desc" : "MA GLOBAL GROUP yuk tashish, bojxona rasmiylashtiruvi va yuklarni tashish sohasida keng ko'lamli xizmatlarni taqdim etadi.Ta'minot zanjiri strategik boshqaruvini ta'minlash orqali biz mijozlarimizning logistikasini iloji boricha samarali qilish orqali ularning raqobatbardoshligini oshiramiz.",
+      "Our-services" : "Bizning xizmatlarimiz",
+      "Our-desc": "Kompaniyamiz tomonidan taqdim etiladigan xizmatlar",
+      "order-text": "Ariza qoldirish",
+      "order-desc": "Bizga so'rov yuboring va maslahat oling",
+      "faq-title": "Ko'p so'raladigan savollar",
+      "contact-title": "Biz bilan bog'laning",
+      "contact-desc": "Biz bilan bog'laning"
+  }
+};
+
+function changeLanguage(language) {
+  document.querySelectorAll('[data-i18n]').forEach(element => {
+      const key = element.getAttribute('data-i18n');
+      element.textContent = translations[language][key];
+  });
+}
+
+// Устанавливаем язык по умолчанию
+changeLanguage('en');
+
